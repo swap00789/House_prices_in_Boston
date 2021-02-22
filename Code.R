f3b9940f1d12000d65256d1f5039baf9e5214815
Swapnil Sakorkar <56971701+swap00789@users.noreply.github.com>
2021-02-21 21:54:10 -0500

    Update Code.R

--- Code.R
+++ Code.R
@@ -1,3 +1,20 @@
+### The analysis begins by understanding the 
+
+CRIM - per capita crime rate by town
+ZN - proportion of residential land zoned for lots over 25,000 sq.ft.
+INDUS - proportion of non-retail business acres per town.
+CHAS - Charles River dummy variable (1 if tract bounds river; 0 otherwise)
+NOX - nitric oxides concentration (parts per 10 million)
+RM - average number of rooms per dwelling
+AGE - proportion of owner-occupied units built prior to 1940
+DIS - weighted distances to five Boston employment centres
+RAD - index of accessibility to radial highways
+TAX - full-value property-tax rate per $10,000
+PTRATIO - pupil-teacher ratio by town
+B where Bk is the proportion of blacks by town
+LSTAT - % lower status of the population
+MEDV - Median value of owner-occupied homes in $1000's
+
 #Install package 'caret'
 install.packages("caret")
 install.packages("formattable")
